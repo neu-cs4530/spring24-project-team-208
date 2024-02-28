db5ffdd25993ca6c57f1feca8c6df83858716fbd
Rayan Yedaly <rayanyedaly@gmail.com>
2024-02-28 17:09:52 -0500

    added documentation/specs for BattleShipGameArea class

---
+++ townService/src/town/games/BattleShipGameArea.ts
@@ -0,0 +1,40 @@
+import GameArea from './GameArea';
+import BattleShipGame from './BattleShipGame';
+import Player from '../../lib/Player';
+import { InteractableType, InteractableCommand, InteractableCommandReturnType } from '../../types/CoveyTownSocket';
+/**
+ * A TicTacToeGameArea is a GameArea that hosts a TicTacToeGame.
+ * @see BattleShipGame
+ * @see GameArea
+ */
+export default class BattleShipGameArea extends GameArea<BattleShipGame> {
+    protected getType(): InteractableType {
+        throw new Error('Method not implemented.');
+    }
+    
+    /**
+   * Handle a command from a player in this game area.
+   * Supported commands:
+   * - JoinGame (joins the game `this._game`, or creates a new one if none is in progress)
+   * - GameMove (applies a move to the game)
+   * - LeaveGame (leaves the game)
+   *
+   * If the command ended the game, records the outcome in this._history
+   * If the command is successful (does not throw an error), calls this._emitAreaChanged (necessary
+   *  to notify any listeners of a state update, including any change to history)
+   * If the command is unsuccessful (throws an error), the error is propagated to the caller
+   *
+   * @see InteractableCommand
+   *
+   * @param command command to handle
+   * @param player player making the request
+   * @returns response to the command, @see InteractableCommandResponse
+   * @throws InvalidParametersError if the command is not supported or is invalid. Invalid commands:
+   *  - LeaveGame and GameMove: No game in progress (GAME_NOT_IN_PROGRESS_MESSAGE),
+   *        or gameID does not match the game in progress (GAME_ID_MISSMATCH_MESSAGE)
+   *  - Any command besides LeaveGame, GameMove and JoinGame: INVALID_COMMAND_MESSAGE
+   */
+    public handleCommand<CommandType extends InteractableCommand>(command: CommandType, player: Player): InteractableCommandReturnType<CommandType> {
+        throw new Error('Method not implemented.');
+    }
+}
