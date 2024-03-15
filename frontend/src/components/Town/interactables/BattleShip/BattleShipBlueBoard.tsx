f979349bdd87dba83b579ff1c16c7d77f78d03ef
Alina Chiu <alinachiu@gmail.com>
2024-03-15 01:35:07 -0700

    Fix linter error

--- frontend/src/components/Town/interactables/BattleShip/BattleShipBlueBoard.tsx
+++ frontend/src/components/Town/interactables/BattleShip/BattleShipBlueBoard.tsx
@@ -31,6 +31,8 @@ export type BattleShipGameProps = {
  *
  * @param gameAreaController the controller for the BattleShip game
  */
-export default function BattleShipBlueBoard({ gameAreaController }: BattleShipGameProps): JSX.Element {
+export default function BattleShipBlueBoard({
+  gameAreaController,
+}: BattleShipGameProps): JSX.Element {
   return <>Implement this to show the blue battleship board</>;
 }
