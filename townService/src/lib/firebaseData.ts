d199d7211b30a873c1f20d81fccb1263adc2815b
Matthew Casey <mattcasey02@gmail.com>
2024-03-24 22:20:16 -0400

    Install firebase and connect to database with admin credentials

---
+++ townService/src/lib/firebaseData.ts
@@ -0,0 +1,15 @@
+import { initializeApp, cert, ServiceAccount } from 'firebase-admin/app';
+import { getFirestore } from 'firebase-admin/firestore';
+import fs from 'fs';
+
+const loadJSON = (path: string) =>
+  JSON.parse(fs.readFileSync(new URL(path, import.meta.url), 'utf8'));
+
+const serviceAccount = loadJSON('../../firebaseAdminKey.json');
+
+initializeApp({
+  credential: cert(<ServiceAccount>serviceAccount),
+});
+
+const db = getFirestore();
+export default db;
