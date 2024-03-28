1c59365df36d334d00480176ceb9adb779fa07ab
Rayan Yedaly <rayanyedaly@gmail.com>
2024-03-28 19:00:12 -0400

    Revert "Normalize line endings"
    This reverts commit a5cd793976f4d114b9ec1e871fea6489dd492be5.

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -33,12 +33,6 @@ module.exports = {
     'no-underscore-dangle': 0,
     'no-param-reassign': 0,
     'no-restricted-syntax': 0,
-    "prettier/prettier": [
-      "error",
-      {
-        "endOfLine": "auto"
-      }
-    ],
     '@typescript-eslint/naming-convention': [
       'error',
       {
