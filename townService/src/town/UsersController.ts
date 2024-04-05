dbb58d30d49c9fbc90f7db80b18e6017b1174184
Matthew Casey <mattcasey02@gmail.com>
2024-04-04 23:34:17 -0400

    Fixed the backend API to properly handle errors and send them over. To handle the errors we need to import the FirebaseError interface from firebase and then set up a type guard. Once we know we have a firebase erorr we can extract the error code and handle the error.
    Also had to add the proper TSOA commands to handle the error and send a 422 message to the frontend.

--- townService/src/town/UsersController.ts
+++ townService/src/town/UsersController.ts
@@ -1,8 +1,15 @@
-import { Body, Controller, Post, Route, Tags, Response } from 'tsoa';
-import InvalidParametersError from '../lib/InvalidParametersError';
+import { Body, Controller, Post, Route, Tags, Response, SuccessResponse } from 'tsoa';
+import { FirebaseError } from 'firebase-admin';
 
 import { UserSignUpParams } from '../api/UserModel';
-import UsersService from './UsersService';
+import { auth } from '../lib/firebaseSetup';
+import InvalidParametersError from '../lib/InvalidParametersError';
+
+function isFirebaseError(error: unknown): error is FirebaseError {
+  return (error as FirebaseError).code !== undefined;
+}
+
+type ErrorMessageResponse = { message: string };
 
 /**
  * This is the user route
@@ -18,12 +25,43 @@ export class UsersController extends Controller {
    * @param request The email, password, and username for the new user
    * @example request {"email": "larrypage@gmail.com", "password": "larrysSuperSecretPassword", "username": "larrypagerocks"}
    */
-  @Response<InvalidParametersError>(
+  @SuccessResponse('201', 'Created')
+  @Response<ErrorMessageResponse>(
     '422',
     'Invalid parameters provided for sign up:\n1. Username is not a nonempty string of at most 128 characters\n2. Username is already taken\n3. Provided email is not a valid email address\n4. An account for this email already exists\n5. Password is too short',
+    { message: 'Provided email is not a valid email address.' },
   )
   @Post('auth/signUp')
   public async signUp(@Body() request: UserSignUpParams): Promise<void> {
-    new UsersService().signUp(request);
+    try {
+      await auth.createUser({
+        uid: request.username,
+        email: request.email,
+        password: request.password,
+      });
+    } catch (error) {
+      if (isFirebaseError(error)) {
+        switch (error.code) {
+          case 'auth/invalid-email':
+            throw new InvalidParametersError('Provided email is not a valid email address.');
+          case 'auth/email-already-exists':
+            throw new InvalidParametersError('An account for this email already exists.');
+          case 'auth/invalid-password':
+            throw new InvalidParametersError(
+              'Password is invalid. It must be at least 6 characters long.',
+            );
+          case 'auth/invalid-uid':
+            throw new InvalidParametersError(
+              'Username is invalid. It must be nonempty and at most 128 characters long.',
+            );
+          case 'auth/uid-already-exists':
+            throw new InvalidParametersError('Username is already taken.');
+          default:
+            throw new Error(error.message);
+        }
+      } else {
+        throw error;
+      }
+    }
   }
 }
