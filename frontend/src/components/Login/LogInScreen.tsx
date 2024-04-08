646061527ab1cdf1e443262b22a45355014f4592
Alina Chiu <alinachiu@gmail.com>
2024-04-08 17:16:46 -0400

    Update login screen to include more distinct styling, Covey.Town logo, and custom color palette

--- frontend/src/components/Login/LogInScreen.tsx
+++ frontend/src/components/Login/LogInScreen.tsx
@@ -12,6 +12,10 @@ import {
   Stack,
   useToast,
   ToastId,
+  HStack,
+  VStack,
+  Image,
+  Spacer,
 } from '@chakra-ui/react';
 import React, { useState } from 'react';
 import useUserLoginController from '../../hooks/useUserLoginController';
@@ -223,86 +227,132 @@ export default function LoginScreen() {
   };
 
   return (
-    <Flex
-      flexDirection='column'
-      width='100wh'
-      height='100vh'
-      backgroundColor='blue.100'
-      justifyContent='center'
-      alignItems='center'>
-      <Stack flexDir='column' mb='2' justifyContent='center' alignItems='center'>
-        <Avatar bg='blue.500' />
-        <Heading color='blue.500'>Welcome to Covey.Town!</Heading>
-        <Text>{showSignUp ? 'Sign up' : 'Login'}</Text>
-        <Box minW={{ base: '90%', md: '468px' }}>
-          <form>
-            <Stack spacing={4} p='1rem' backgroundColor='whiteAlpha.900' boxShadow='md'>
-              {showSignUp && (
-                <>
+    <Flex minH='100vh'>
+      <Box flex='3' w='full' h='100vh' bg='#81C8FB'>
+        <Flex minH='100vh'>
+          <VStack w='full' h='100vh'>
+            <Box w='full' h='10%' marginLeft='10%' marginTop='2.5%'>
+              <HStack spacing={5}>
+                <Box boxSize='5vh'>
+                  <Image src='logo192.png' alt='Covey.Town logo' />
+                </Box>
+                <Text fontWeight='bold' fontSize='2em' color='gray.600'>
+                  {' '}
+                  Covey.Town{' '}
+                </Text>
+              </HStack>
+            </Box>
+            <Box w='full' h='100%' display='flex' alignItems='center' justifyContent='center'>
+              <Text
+                fontSize={{ base: '3.3em', sm: '2.5em', md: '4em', lg: '5.3em' }}
+                fontWeight='650'
+                marginBottom='25%'
+                lineHeight='115%'
+                color='white'
+                paddingLeft={'10px'}
+                paddingBottom={{ base: '0px', sm: '100px', md: '80px', lg: '0px' }}>
+                {' '}
+                A very cool video <br /> chat app.{' '}
+              </Text>
+            </Box>
+          </VStack>
+        </Flex>
+      </Box>
+      <Box flex='3' w='full' h='100vh' bg='white'>
+        <Flex
+          flexDirection='column'
+          width='100wh'
+          height='100vh'
+          backgroundColor='white'
+          justifyContent='center'
+          alignItems='center'>
+          <Stack flexDir='column' mb='2' justifyContent='center'>
+            <Box textAlign='start' mb='-0.25rem' ms={2}>
+              <Heading fontSize='2em' color='gray.400'>
+                Welcome!
+              </Heading>
+              <Heading fontSize='2em' color='gray.600'>
+                {showSignUp ? 'Sign up to join our community.' : 'Login to get started.'}
+              </Heading>
+            </Box>
+            <Spacer />
+            <Spacer />
+            <Box minW={{ base: '90%', md: '500px' }}>
+              <form>
+                <Stack spacing={4} p='1rem' backgroundColor='whiteAlpha.900' boxShadow='lg'>
+                  {showSignUp && (
+                    <>
+                      <FormControl>
+                        <InputGroup>
+                          <Input
+                            type='text'
+                            placeholder='Username'
+                            value={username}
+                            onChange={e => setUsername(e.target.value)}
+                          />
+                        </InputGroup>
+                      </FormControl>
+                    </>
+                  )}
                   <FormControl>
                     <InputGroup>
                       <Input
-                        type='text'
-                        placeholder='Username'
-                        value={username}
-                        onChange={e => setUsername(e.target.value)}
+                        type='email'
+                        placeholder='Email address'
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
                       />
                     </InputGroup>
                   </FormControl>
-                </>
-              )}
-              <FormControl>
-                <InputGroup>
-                  <Input
-                    type='email'
-                    placeholder='Email address'
-                    value={email}
-                    onChange={e => setEmail(e.target.value)}
-                  />
-                </InputGroup>
-              </FormControl>
-              <FormControl>
-                <InputGroup>
-                  <Input
-                    type={showPassword ? 'text' : 'password'}
-                    placeholder='Password'
-                    value={password}
-                    onChange={e => setPassword(e.target.value)}
-                  />
-                  <InputRightElement width='4.5rem'>
-                    <Button h='1.75rem' size='sm' onClick={handleShowPasswordClick}>
-                      {showPassword ? 'Hide' : 'Show'}
+                  <FormControl>
+                    <InputGroup>
+                      <Input
+                        type={showPassword ? 'text' : 'password'}
+                        placeholder='Password'
+                        value={password}
+                        onChange={e => setPassword(e.target.value)}
+                      />
+                      <InputRightElement width='4.5rem'>
+                        <Button h='1.75rem' size='sm' onClick={handleShowPasswordClick}>
+                          {showPassword ? 'Hide' : 'Show'}
+                        </Button>
+                      </InputRightElement>
+                    </InputGroup>
+                  </FormControl>
+                  {!showSignUp && (
+                    <Button
+                      bg='#81C8FB'
+                      color='white'
+                      onClick={handleLoginButton}
+                      isLoading={isLoggingIn}
+                      isDisabled={isLoggingIn}
+                      variant='solid'
+                      _hover={{ bg: '#33AAFF' }}>
+                      Login
                     </Button>
-                  </InputRightElement>
-                </InputGroup>
-              </FormControl>
-              {!showSignUp && (
-                <Button
-                  colorScheme='blue'
-                  onClick={handleLoginButton}
-                  isLoading={isLoggingIn}
-                  isDisabled={isLoggingIn}>
-                  Login
-                </Button>
-              )}
-              {showSignUp && (
-                <Button
-                  colorScheme='blue'
-                  onClick={handleSignUpButton}
-                  isLoading={isSigningUp}
-                  isDisabled={isSigningUp}>
-                  Sign Up
-                </Button>
-              )}
-            </Stack>
-          </form>
-        </Box>
-      </Stack>
-      <Box>
-        Don&apos;t have an account?{' '}
-        <Button size='xs' colorScheme='gray' onClick={handleShowSignUp}>
-          {showSignUp ? 'Login' : 'Sign up'}
-        </Button>
+                  )}
+                  {showSignUp && (
+                    <Button
+                      bg='#81C8FB'
+                      color='white'
+                      onClick={handleSignUpButton}
+                      isLoading={isSigningUp}
+                      isDisabled={isSigningUp}
+                      _hover={{ bg: '#33AAFF' }}>
+                      Sign Up
+                    </Button>
+                  )}
+                </Stack>
+              </form>
+            </Box>
+          </Stack>
+          <Box>
+            Don&apos;t have an account?{' '}
+            <Button size='xs' colorScheme='gray' onClick={handleShowSignUp}>
+              {showSignUp ? 'Login' : 'Sign up'}
+            </Button>
+          </Box>
+        </Flex>
       </Box>
     </Flex>
   );
